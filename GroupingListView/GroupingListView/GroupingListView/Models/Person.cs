5877309dd0f41516967eedfdb3a379bf03e092e4
Sebastian Jensen <apps@tsjdev-apps.de>
2019-04-16 16:00:09 +0200

    - add data models

---
+++ GroupingListView/GroupingListView/GroupingListView/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace GroupingListView.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ GroupingListView/GroupingListView/GroupingListView/Models/PersonGroup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace GroupingListView.Models
+{
+    public class PersonGroup : List<Person>
+    {
+        public string Title { get; set; }
+        public string ShortName { get; set; }
+
+
+        public PersonGroup(string title, string shortName)
+        {
+            Title = title;
+            ShortName = shortName;
+        }
+    }
+}
