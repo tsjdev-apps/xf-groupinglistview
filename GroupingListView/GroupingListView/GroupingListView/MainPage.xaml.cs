656f7284b4316498d5b59e50fadc0542cd4583e8
Sebastian Jensen <apps@tsjdev-apps.de>
2019-04-16 16:01:02 +0200

    - show grouping listview on mainpage

--- GroupingListView/GroupingListView/GroupingListView/MainPage.xaml.cs
+++ GroupingListView/GroupingListView/GroupingListView/MainPage.xaml.cs
@@ -1,21 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GroupingListView.ViewModels;
 using Xamarin.Forms;
 
 namespace GroupingListView
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
-    [DesignTimeVisible(true)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            ((MainPageViewModel)BindingContext).LoadPersonGroupsCommand.Execute(null);
         }
     }
 }
